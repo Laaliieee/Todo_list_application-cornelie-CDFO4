9c3816f3f86682c51905d61ce1cd60f8a4aad17c
Joalie CORNELIE <joalie.cornelie@gmail.com>
2024-01-29 14:13:09 +0100

    Creation of the application and what must be done after

--- todo_list.py
+++ todo_list.py
@@ -25,4 +25,44 @@ class TodoList:
             for i, task in enumerate(self.tasks, start=1):
                 print(f"{i}. {task}")
 
+def main():
+    todo_list = TodoList()
 
+    while True:
+        print("\n===== Todo List Menu =====")
+        print("1. Add Task")
+        print("2. Remove Task")
+        print("3. Display Tasks")
+        print("4. Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            task = input("Enter the task: ")
+            todo_list.add_task(task)
+            print("Task added.")
+        elif choice == "2":
+            task = input("Enter the task to remove: ")
+            try:
+                todo_list.remove_task(task)
+                print("Task removed.")
+            except ValueError:
+                print("Task not found.")
+        elif choice == "3":
+            print("\n===== Tasks =====")
+            todo_list.display_tasks()
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#The application is functionnal but i want to add more features 
+# I want that the users can evaluate the progress of the task like 'in progress' 'started' 
+# I want that the users can classify the importance of the task like 'can be done later or after' 'should be done immediately' 
