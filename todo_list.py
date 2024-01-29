f48ad7cbe9bd0a3dcba50ec2726e0388d7efa179
dylandray <120407558+dylandray@users.noreply.github.com>
2024-01-29 16:24:38 +0100

    Update todo_list.py
    Adding importance

--- todo_list.py
+++ todo_list.py
@@ -8,12 +8,17 @@ Created on Mon Jan 29 14:05:00 2024
 class TodoList:
     def __init__(self):
         self.tasks = []
+        self.importance = []
 
-    def add_task(self, task):
+    def add_task(self, task, importance):
         self.tasks.append(task)
+        self.importance.append(importance)
 
     def remove_task(self, task):
-        self.tasks.remove(task)
+        if task in self.tasks:
+            index = self.tasks.index(task)
+            self.tasks.pop(index)
+            self.importance.pop(index)
 
     def get_tasks(self):
         return self.tasks
@@ -22,8 +27,8 @@ class TodoList:
         if not self.tasks:
             print("No tasks.")
         else:
-            for i, task in enumerate(self.tasks, start=1):
-                print(f"{i}. {task}")
+            for i, (task, importance) in enumerate(zip(self.tasks, self.importance), start=1):
+                print(f"{i}. {task}, {importance}")
 
 def main():
     todo_list = TodoList()
@@ -39,8 +44,17 @@ def main():
 
         if choice == "1":
             task = input("Enter the task: ")
-            todo_list.add_task(task)
+            print("Please select the importance of your task")
+            print("1. Not that important")
+            print("2. Important")
+            print("3. Extremely important")
+            importance= int(input("Choose the importance of your task: "))
+            while importance != 1 and importance != 2 and importance !=3:
+                print("You should type 1 (Not that important), 2 (Important) or 3 (Extremely important).")
+                importance= int(input("Choose the importance of your task: "))
+            todo_list.add_task(task, importance)
             print("Task added.")
+            
         elif choice == "2":
             task = input("Enter the task to remove: ")
             try:
@@ -48,8 +62,9 @@ def main():
                 print("Task removed.")
             except ValueError:
                 print("Task not found.")
-        elif choice == "4":
+        elif choice == "3":
             print("\n===== Tasks =====")
+            print("===== 1: Not that important, 2: Important, 3: Extremely important =====")
             todo_list.display_tasks()
         elif choice == "4":
             print("Goodbye!")
